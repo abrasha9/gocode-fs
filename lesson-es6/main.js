493b7622748d8e3849c97f92968c5b588c9b4b7d
abrasha <abrasha9@gmail.com>
2022-12-19 01:39:58 +0200

    b

--- lesson-es6/main.js
+++ lesson-es6/main.js
@@ -1,22 +1,19 @@
 //Use destructuring to make this code cleaner
-
-function detectCollision(objects,point) {
+git
+function detectCollision(objects,{x:px,y:py}) {
     for (let i = 0; i < objects.length; i++) {
       let object = objects[i];
       const {x,y,width,height} = objects[i];
-      const {x:px,y:py} = point;
       if (
         px >= x &&
         px <= x + width &&
         py >= y &&
         py <= y + height
       ){
-      console.log('i was here '+object);
       return object;}
     }
   }
- 
-  
+itit
   const myObjects = [
     { x: 10, y: 20, width: 30, height: 30 },
     { x: -40, y: 20, width: 30, height: 30 },
@@ -25,5 +22,4 @@ function detectCollision(objects,point) {
 
   const point =  { x:10, y:20};
   
-
   console.log(detectCollision(myObjects,point));
