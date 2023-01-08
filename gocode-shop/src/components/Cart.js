b9f25b52f433258127d70f2205ef2c20c8dafcc5
unknown <you@example.com>
2023-01-08 20:54:06 +0200

    finished cart with amount +-

--- gocode-shop/src/components/Cart.js
+++ gocode-shop/src/components/Cart.js
@@ -4,12 +4,32 @@ import {MyContext} from '../MyContext';
 
 const Cart = () => {
   const dataFromContext = useContext(MyContext);
+
+  const removeItem = (id,command) =>{
+    console.log(id);
+    let position = dataFromContext.cartList.findIndex((el)=>( el.id === id))
+    if (command === 'remove'){
+      dataFromContext.cartList[position].amount--;
+    } else {
+      dataFromContext.cartList[position].amount++;
+
+    }
+  if (dataFromContext.cartList[position].amount === 0){
+    dataFromContext.cartList.splice(position,1);
+  }
+    dataFromContext.setCartList([...dataFromContext.cartList]); 
+  }
+
   return (
     <div className='cartbox'>
      <h1> Your Cart:</h1> 
      {dataFromContext.cartList.map((prop) => 
-      <p key={prop.id} 
-      title={prop.name}>{prop.name} price: {prop.price}₪ amount: {prop.amount}<br></br><br></br></p>)}
+      <p key={prop.id} id={prop.id}
+      title={prop.name}>{prop.name} price: {(prop.price*prop.amount).toFixed(2)} ₪ amount: {prop.amount}<br></br>
+      <button onClick={()=>removeItem(prop.id,'remove')}>-</button>
+      <button onClick={()=>removeItem(prop.id,'add')}>+</button>
+      
+      <br></br></p>)}
       
      </div>
   )
